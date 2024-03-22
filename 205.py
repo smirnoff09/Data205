69e632b27a010e94595bbe87af3ccd55c3d24afe
smirnoff09 <98981609+smirnoff09@users.noreply.github.com>
2024-03-22 16:35:15 -0400

    Update 205.py

--- 205.py
+++ 205.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 
 
-
+#import
 df = pd.DataFrame(requests.get('https://data.cdc.gov/resource/swc5-untb.json').json()),
 display(df)
 pd.set_option('display.max_columns', None)
 
+#desperate attempt at making something work
 df1=df
 
+#trying to make first row a header
 df1.rename(columns=df.iloc[0]).drop(df.index[0])
 
 
